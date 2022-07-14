da7a6a7cf9027fad42afefc96cbf00fc4ef8ad47
Abylay Aiyp <ablyaip@gmail.com>
2022-07-14 21:11:39 +0600

    done: task 2

--- task_2/index.jsx
+++ task_2/index.jsx
@@ -1,16 +1,30 @@
-import { Fragment, memo } from 'react';
+// ФАЙЛ ДЛЯ РЕДАКТИРОВАНИЯ И ТЕСТИРОВАНИЯ КОМПОНЕНТОВ ИЗ ТЕСТОВОГО ЗАДАНИЯ
 
-const MainComponent = () => {
-    const makeLog = () => console.log('hi from MainComponent'); // function to make logs from MainComponent
+import React, { useState, Fragment, memo, useCallback } from "react";
 
-    return (
-        <Fragment>
-            <ChildComponent makeLog={makeLog} />
-        </Fragment>
-    );
-};
+import { IProps } from "./interface";
 
 // memoized component
-const ChildComponent = memo(({ makeLog }) => (
-    <button onClick={makeLog}>say Hi from ChildComponent</button>
-));
+const ChildComponent = memo(({ makeLog }: IProps) => {
+  console.log("ChildComponent has been updated. Memoization is not working.");
+  return (
+    <button className="btn btn-info" onClick={makeLog}>
+      say Hi from ChildComponent
+    </button>
+  );
+});
+
+export const MainComponent = () => {
+  const [state, setState] = useState < boolean > false; // change state for component force updating
+
+  const makeLog = useCallback(() => console.log("hi from MainComponent"), []); // function to make a log from MainComponent
+
+  return (
+    <Fragment>
+      <button className="btn btn-info btn-lg" onClick={() => setState(!state)}>
+        MainComponent force updating
+      </button>
+      <ChildComponent makeLog={makeLog} />
+    </Fragment>
+  );
+};

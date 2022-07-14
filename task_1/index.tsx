8c315a0da7f615dc2da2cf5907367ca46078c7f5
Abylay Aiyp <ablyaip@gmail.com>
2022-07-14 20:47:18 +0600

    done: task 1

--- task_1/index.tsx
+++ task_1/index.tsx
@@ -1,46 +1,61 @@
-import { Component } from 'react';
+import { React, Component, memo, useMemo, PureComponent } from "react";
 
 type IUser = {
-    name: string
-    age: number
-}
+  name: string;
+  age: number;
+};
 
 type IProps = {
-    user: IUser
-}
+  user: IUser;
+};
 
 // functional component
-const FirstComponent = ({ name, age }: IUser) => (
+const FirstComponent = memo(({ name, age }: IUser) => {
+  console.log("FirstComponent has been updated");
+  return (
     <div>
-        my name is {name}, my age is {age}
+      my name is {name}, my age is {age}
     </div>
-);
+  );
+});
 
 // functional component
-const SecondComponent = ({ user: { name, age } }: IProps) => (
-    <div>
+const SecondComponent = ({ user: { name, age } }: IProps) => {
+  return useMemo(() => {
+    console.log("SecondComponent has been updated");
+    return (
+      <div>
         my name is {name}, my age is {age}
-    </div>
-);
+      </div>
+    );
+  }, [name, age]);
+};
 
 // class component
-class ThirdComponent extends Component<IUser> {
-    render() {
-        return (
-            <div>
-                my name is {this.props.name}, my age is {this.props.age}
-            </div>
-        )
-    }
+class ThirdComponent extends PureComponent<IUser> {
+  render() {
+    console.log("ThirdComponent has been updated");
+
+    return (
+      <div>
+        my name is {this.props.name}, my age is {this.props.age}
+      </div>
+    );
+  }
 }
 
 // class component
-class FourthComponent extends Component<IProps> {
-    render() {
-        return (
-            <div>
-                my name is {this.props.user.name}, my age is {this.props.user.age}
-            </div>
-        )
-    }
+class FourthComponent extends PureComponent<IProps> {
+  shouldComponentUpdate(nextProps, nextState) {
+    return JSON.stringify(this.props.user) !== JSON.stringify(nextProps.user);
+  }
+  render() {
+    console.log("FourthComponent has been updated");
+
+    return (
+      <div>
+        my name is {this.props.user.name}, my age is {this.props.user.age}
+      </div>
+    );
+  }
 }

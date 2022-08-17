90e9a56cf083f60cb254fed620fff5691cd82a0f
Shakhzod Jumaev <shakhzod_jumaev@epam.com>
2022-08-17 06:19:34 +0500

    all tasks are done

--- task_5/index.jsx
+++ task_5/index.jsx
@@ -1,21 +1,44 @@
 import { useState } from "react";
+// ФАЙЛ ДЛЯ РЕДАКТИРОВАНИЯ И ТЕСТИРОВАНИЯ КОМПОНЕНТОВ ИЗ ТЕСТОВОГО ЗАДАНИЯ
 
-export const Block1 = ({ mouseEnterCallbak, imgSrc, imgAlt }) => {
-  const [isActive, setActive] = useState(false);
+import { useState } from "react";
 
-  const mouseEnterHandler = () => {
-    setActive(true);
-    mouseEnterCallbak();
-  };
+import { CommonBlock } from "./taskComponents";
 
+const infoArr = [
+  {
+    id: 1,
+    mouseEnterCallbak: () => console.log("hi from Block 1"),
+    imgSrc:
+      "https://ohmylook.ua/files/products/42504.290x484.JPG?ce7d3c50d2e66b146f8711dd9eb7af35",
+    imgAlt: "my picture",
+  },
+  {
+    id: 2,
+    mouseEnterCallbak: () => console.log("hi from Block 2"),
+    content: "Magdalena",
+  },
+  {
+    id: 3,
+    mouseEnterCallbak: () => console.log("hi from Block 3"),
+    userData: { country: "USA", street: "Maskavas" },
+  },
+];
+
+function App() {
   return (
-    <div onMouseEnter={mouseEnterHandler} className={isActive ? "active" : ""}>
-      <img src={imgSrc} alt={imgAlt} />
-    </div>
+    <Fragment>
+      {infoArr.map((item) => (
+        <CommonBlock key={item.id} item={item} />
+      ))}
+    </Fragment>
   );
-};
+}
 
-export const Block2 = ({ mouseEnterCallbak, content }) => {
+export const CommonBlock = (props) => {
+  const { item } = props;
+  const { mouseEnterCallbak, userData, content, imgSrc, imgAlt } = item;
+  // console.log(item);
   const [isActive, setActive] = useState(false);
 
   const mouseEnterHandler = () => {
@@ -25,24 +48,13 @@ export const Block2 = ({ mouseEnterCallbak, content }) => {
 
   return (
     <div onMouseEnter={mouseEnterHandler} className={isActive ? "active" : ""}>
-      <p>{content}</p>
-    </div>
-  );
-};
-
-export const Block3 = ({ mouseEnterCallbak, userData }) => {
-  const [isActive, setActive] = useState(false);
-
-  const mouseEnterHandler = () => {
-    setActive(true);
-    mouseEnterCallbak();
-  };
-
-  return (
-    <div onMouseEnter={mouseEnterHandler} className={isActive ? "active" : ""}>
-      <address>
-        country: {userData.country}, street: {userData.street}
-      </address>
+      {imgSrc && imgAlt && <img src={imgSrc} alt={imgAlt} />}
+      {content && <p>{content}</p>}
+      {userData && (
+        <address>
+          country: {userData.country}, street: {userData.street}
+        </address>
+      )}
     </div>
   );
 };

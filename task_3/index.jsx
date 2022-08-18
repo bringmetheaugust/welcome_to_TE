ceee2493ca825d861972857a62a007590f268b56
Alexander <bdiyoz@gmail.com>
2022-08-18 19:03:50 +0400

    feat: complite third task

--- task_3/index.jsx
+++ task_3/index.jsx
@@ -1,5 +1,16 @@
 import { Fragment, memo } from 'react';
 
+const isPropsEqual = (prevProps, nextProps) => {
+    if (
+      prevProps.user.name === nextProps.user.name ||
+      prevProps.user.age === nextProps.user.age
+    ) {
+      return true;
+    }
+  
+    return false;
+  };
+
 const MainComponent = ({
     user = { name: 'unknown', age: null } // default value for `props.user`
 }) => {
@@ -15,4 +26,4 @@ const ChildComponent = memo(({ user: { name, age } }) => {
     return (
         <div>user name: {name}, user age: {age}</div>
     )
-});
+}, isPropsEqual);

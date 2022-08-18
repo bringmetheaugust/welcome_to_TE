60bb7b7f78cde2dfed37c3c87c31e077326da00f
Alexander <bdiyoz@gmail.com>
2022-08-19 00:19:08 +0400

    feat: complite fourth task

--- task_4/index.jsx
+++ task_4/index.jsx
@@ -1,4 +1,4 @@
-import { Component, createRef } from 'react';
+import { Component, createRef, useState, forwardRef, useImperativeHandle } from 'react';
 
 class MainComponent extends Component {
     myRef = createRef(); // create simple ref
@@ -15,14 +15,14 @@ class MainComponent extends Component {
     }
 };
 
-class ChildComponent extends Component {
-    state = { isActive: true };
-
-    toggleButton = () => this.setState({ isActive: !this.state.isActive });
-
-    render() {
-        return (
-            this.state.isActive ? <div>child component</div> : null
-        );
-    }
-};
+const ChildComponent = forwardRef((props, ref) => {
+    const [isActive, setIsActive] = useState(true);
+  
+    useImperativeHandle(ref, () => ({
+      toggleButton() {
+        setIsActive((prev) => !prev);
+      }
+    }));
+  
+    return <>{isActive && <div>child component</div>}</>;
+  });
